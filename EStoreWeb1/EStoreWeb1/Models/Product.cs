8073ad508eb42fe2c8e4fc73fa53d6255ecbe5ed
LeeNhuNghia <lnghia637@gmail.com>
2023-09-20 19:42:29 +0700

    Thay doi 2

---
+++ EStoreWeb1/EStoreWeb1/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EStoreWeb1.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public double Price { get; set; }
+        public int CategoryId { get; set; }
+
+        // Khai báo mối kết hợp 1-n
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
